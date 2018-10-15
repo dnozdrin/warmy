38331d4b351cfdf1cbe070ff35a0bb1e54349a30
Dmytro Nozdrin <mail@nozdrin.net>
2018-10-15 15:20:38 +0300

    [TASK] Refactor temperature sensor class

--- app/Library/Sensor.php
+++ app/Library/Sensor.php
@@ -10,7 +10,7 @@ class Sensor
     /**
      * @var string
      */
-    protected $sensor;
+    private $sensorPath;
 
     /**
      * Sensor constructor.
@@ -18,27 +18,27 @@ class Sensor
      */
     public function __construct()
     {
-        $sensorId = config('app.sensor_id', false);
-        if ($sensorId === false) {
+        $sensorId = config('app.sensor_id', '');
+        if ($sensorId === '') {
             throw new \Exception('Sensor ID was not configured!', 1532625136);
         }
 
-        $this->sensor = "/sys/devices/w1_bus_master1/{$sensorId}/w1_slave";
+        $this->sensorPath = "/sys/devices/w1_bus_master1/{$sensorId}/w1_slave";
     }
 
     /**
      * @return int
      * @throws \Exception
      */
-    public function temperature(): int
+    private function temperature(): int
     {
-        $sensorResponse = file($this->sensor);
+        $sensorResponse = file($this->sensorPath);
         if ($sensorResponse === false) {
             throw new \Exception('Unable to read data from sensor!', 1532712918);
         }
-        $sensorData = explode('=', $sensorResponse[1]);
+        $temperature = explode('=', $sensorResponse[1]);
 
-        return (int)$sensorData[1];
+        return (int)$temperature[1];
     }
 
     /**
